99ee3d84c5a5b0f98cc0e030fe793b053c6c73da
Nigel Rook <nigel@electricimp.com>
2017-12-02 16:19:42 +0000

    Make tests actually test (they fail)

--- test_smartclimate.py
+++ test_smartclimate.py
@@ -52,10 +52,14 @@ class TestSmartClimate(unittest.TestCase):
         self.hass.stop()
         self.cleanup()
 
-    @coroutine
     def init(self, config):
         '''Init smartclimate component'''
-        yield from async_setup_component(self.hass, 'smartclimate', config)
+        return run_coroutine_threadsafe(
+            async_setup_component(self.hass, 'smartclimate', config), self.hass.loop).result()
+
+    def block_till_done(self):
+        '''Block the hass loop until everything is processed'''
+        return run_coroutine_threadsafe(self.hass.block_till_done, self.hass.loop).result()
 
     @staticmethod
     def relative_time(time_s):
@@ -79,29 +83,54 @@ class TestSmartClimate(unittest.TestCase):
             pickle.dump(data, file)
         return data
 
-    @coroutine
+    def test_simple_config_validation(self):
+        '''Test validation of a valid, complex config'''
+        smartclimate.CONFIG_SCHEMA(SIMPLE_CONFIG)
+
+    def test_complex_config_validation(self):
+        '''Test validation of a valid, complex config'''
+        config = {
+            'smartclimate': {
+                'test1': {
+                    'entity_id': ENTITY_ID,
+                    'sensors': [
+                        {
+                            'entity_id': 'sensor.sensor1'
+                        },
+                        {
+                            'entity_id': 'sensor.sensor2',
+                            'attribute': 'attr'
+                        }
+                    ]
+                },
+                'test2': {
+                    'entity_id': 'climate.test2'
+                }
+            }
+        }
+        smartclimate.CONFIG_SCHEMA(config)
+
     def test_initial_setup(self):
         '''Test the setup.'''
-        response = yield from async_setup_component(self.hass, 'smartclimate', SIMPLE_CONFIG)
+        response = self.init(SIMPLE_CONFIG)
         self.assertTrue(response)
 
-    @coroutine
     @unittest.mock.patch('homeassistant.util.dt.utcnow')
     def test_records_temperature_change(self, mock):
         '''Test a completed temperature shift gets recorded to the store'''
-        yield from self.init(SIMPLE_CONFIG)
+        self.init(SIMPLE_CONFIG)
 
         old_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 18., 'current_temperature' : 18.})
         new_state = State(ENTITY_ID, 'Manual', {'temperature': 20., 'current_temperature' : 18.})
         mock.return_value = self.relative_time(0)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         old_state = new_state
         new_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 20., 'current_temperature' : 20.})
         mock.return_value = self.relative_time(5100)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         self.assertTrue(os.path.isfile(self.store))
         with open(self.store, 'rb') as file:
@@ -118,52 +147,50 @@ class TestSmartClimate(unittest.TestCase):
                 }
             })
 
-    @coroutine
     @unittest.mock.patch('homeassistant.util.dt.utcnow')
     def test_aborts_test_if_target_temp_reset(self, mock):
         '''Test a completed temperature shift gets recorded to the store'''
         initial_data = self.simple_init_data()
-        yield from self.init(SIMPLE_CONFIG)
+        self.init(SIMPLE_CONFIG)
 
         old_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 18., 'current_temperature' : 18.})
         new_state = State(ENTITY_ID, 'Manual', {'temperature': 19., 'current_temperature' : 18.})
         mock.return_value = self.relative_time(0)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         old_state = new_state
         new_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 18., 'current_temperature' : 18.5})
         mock.return_value = self.relative_time(2400)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         with open(self.store, 'rb') as file:
             data = pickle.load(file)
             self.assertEqual(data, initial_data)
 
-    @coroutine
     @unittest.mock.patch('homeassistant.util.dt.utcnow')
     def test_ignores_intermediate_states(self, mock):
         '''Test a completed temperature shift gets recorded to the store'''
-        yield from self.init(SIMPLE_CONFIG)
+        self.init(SIMPLE_CONFIG)
 
         old_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 18., 'current_temperature' : 18.})
         new_state = State(ENTITY_ID, 'Manual', {'temperature': 20., 'current_temperature' : 18.})
         mock.return_value = self.relative_time(0)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         old_state = new_state
         new_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 20., 'current_temperature' : 19.})
         mock.return_value = self.relative_time(5100)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         old_state = new_state
         new_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 20., 'current_temperature' : 20.})
         mock.return_value = self.relative_time(5100)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         self.assertTrue(os.path.isfile(self.store))
         with open(self.store, 'rb') as file:
@@ -180,7 +207,6 @@ class TestSmartClimate(unittest.TestCase):
                 }
             })
 
-    @coroutine
     @unittest.mock.patch('homeassistant.util.dt.utcnow')
     def test_records_additional_sensors(self, mock):
         '''Test a completed temperature shift gets recorded to the store'''
@@ -198,7 +224,7 @@ class TestSmartClimate(unittest.TestCase):
                 ]
             }
         }
-        yield from self.init(config)
+        self.init(config)
 
         self.hass.states.set('sensor.sensor1', '8.0')
         self.hass.states.set('sensor.sensor2', 'on', {'attr': '16.0'})
@@ -206,7 +232,7 @@ class TestSmartClimate(unittest.TestCase):
         new_state = State(ENTITY_ID, 'Manual', {'temperature': 20., 'current_temperature' : 18.})
         mock.return_value = self.relative_time(0)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         self.hass.states.set('sensor.sensor1', '9.0')
         self.hass.states.set('sensor.sensor2', 'on', {'attr': '17.0'})
@@ -214,7 +240,7 @@ class TestSmartClimate(unittest.TestCase):
         new_state = State(ENTITY_ID, 'Smart Schedule', {'temperature': 20., 'current_temperature' : 20.})
         mock.return_value = self.relative_time(5100)
         mock_state_change_event(self.hass, new_state, old_state)
-        self.hass.block_till_done()
+        self.block_till_done()
 
         self.assertTrue(os.path.isfile(self.store))
         with open(self.store, 'rb') as file:
