77e87e90fb3a24a63313d1197f915bde70c97a96
Nigel Rook <nigelrook@gmail.com>
2019-11-29 21:38:44 +0000

    Fix name collision with HassLog

--- smartclimate/hasslog.py
+++ smartclimate/hasslog.py
@@ -1,21 +1,21 @@
 import logging
 
 class HassLog:
-    def __init__(self, hass):
-        self.hass = hass
+    def __init__(self, app):
+        self._app = app
 
     def debug(self, message, *args, **kwargs):
-        if self.hass.get_main_log().isEnabledFor(logging.DEBUG):
-            self.hass.log(message.format(*args, **kwargs), level="DEBUG")
+        if self._app.get_main_log().isEnabledFor(logging.DEBUG):
+            self._app.log(message.format(*args, **kwargs), level="DEBUG")
 
     def info(self, message, *args, **kwargs):
-        if self.hass.get_main_log().isEnabledFor(logging.INFO):
-            self.hass.log(message.format(*args, **kwargs), level="INFO")
+        if self._app.get_main_log().isEnabledFor(logging.INFO):
+            self._app.log(message.format(*args, **kwargs), level="INFO")
 
     def warning(self, message, *args, **kwargs):
-        if self.hass.get_main_log().isEnabledFor(logging.WARNING):
-            self.hass.log(message.format(*args, **kwargs), level="WARNING")
+        if self._app.get_main_log().isEnabledFor(logging.WARNING):
+            self._app.log(message.format(*args, **kwargs), level="WARNING")
 
     def error(self, message, *args, **kwargs):
-        if self.hass.get_main_log().isEnabledFor(logging.ERROR):
-            self.hass.log(message.format(*args, **kwargs), level="ERROR")
+        if self._app.get_main_log().isEnabledFor(logging.ERROR):
+            self._app.log(message.format(*args, **kwargs), level="ERROR")
